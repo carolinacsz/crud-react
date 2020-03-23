0096bf6d44253574aa50e869120b304d5a1f1cee
Carolina Carvalho <carvalhosouza97@gmail.com>
2020-03-23 13:43:25 +0100

    First Commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Home"
+        subtitle="React Project.">
+        <div className='display-4'>Welcome!</div>
+        <hr />
+        <p className="mb-0">System to exemplify a construction
+of a register developed with React!</p>
+    </Main>
